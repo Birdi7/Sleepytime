09bb60e70b47eb307660ab8991fd45800834b7de
Егор Осокин <egorr.osokin@gmail.com>
2016-07-08 21:11:48 +0500

    Show TimePicker dialog

--- app/src/main/java/com/sleepytime/MainActivity.java
+++ app/src/main/java/com/sleepytime/MainActivity.java
@@ -1,23 +1,24 @@
 package com.sleepytime;
 
-import android.content.Intent;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    boolean is24hour;
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity
+                          implements TimePickerDialog.OnTimeSetListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        is24hour = DateFormat.is24HourFormat(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,14 +44,23 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClickHaveToWakeUp(View view) {
-
+    public void onClickSetTime(View view) {
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog dialog = new TimePickerDialog(
+                MainActivity.this,
+                this,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                DateFormat.is24HourFormat(this));
+        dialog.show();
     }
-
+    public void onTimeSet(TimePicker picker, int hour, int minutes) {
+        Toast.makeText(MainActivity.this, String.valueOf(hour) +"\n"+ String.valueOf(minutes), Toast.LENGTH_SHORT).show();
+    }
 }
 /*
  HOW TO SET ALARM:
     Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-intent.putExtra(AlarmClock.EXTRA_HOUR, 23);
-        intent.putExtra(AlarmClock.EXTRA_MINUTES, 22);
-        startActivity(intent);*/
+    intent.putExtra(AlarmClock.EXTRA_HOUR, 23);
+    intent.putExtra(AlarmClock.EXTRA_MINUTES, 22);
+    startActivity(intent);*/
